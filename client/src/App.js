44852273379dfe6717c3e2b0ed469edff198fc2e
tanmay <tanmaypatiltp25@gmail.com>
2023-04-26 11:01:46 +0530

    added functionality to edit tasks

---
+++ client/src/App.js
@@ -0,0 +1,126 @@
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import Todo from "./Todo";
+
+const API_BASE = "http://localhost:3001";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [newTodo, SetNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+    console.log(todos);
+  }, []);
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(API_BASE + "/todo/delete/" + id, {
+      method: "DELETE",
+    }).then((res) => res.json());
+    setTodos((todos) => todos.filter((todo) => todo._id !== data._id));
+  };
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ text: newTodo }),
+    }).then((res) => res.json());
+    setTodos([...todos, data]);
+    toast.success("Task Added!");
+    SetNewTodo("");
+  };
+
+  const completeTodo = async (id) => {
+    const data = await fetch(API_BASE + "/todo/complete/" + id).then((res) =>
+      res.json()
+    );
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.complete = data.complete;
+        }
+        return todo;
+      })
+    );
+    toast.success("Task Marked as Completed!");
+  };
+
+  const editTodo = async (id, newText) => {
+    console.log(id, newText);
+    const data = await fetch(API_BASE + "/todo/edit/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newText }),
+    }).then((res) => res.json());
+
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.text = data.text;
+        }
+        return todo;
+      })
+    );
+    toast.success("Task Updated!");
+  };
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then((res) => res.json()) // return the parsed JSON response
+      .then((data) => {
+        setTodos(data);
+        console.log(data);
+      })
+      .catch((err) => console.error("Locha", err));
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <Toaster />
+      </div>
+      <h1 className="text-center text-2xl font-bold text-white">Todo List</h1>
+
+      <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div className="mb-4">
+            <h1 className="text-black font-bold">Your Tasks</h1>
+            <div className="flex mt-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                placeholder="Add Todo"
+                onChange={(e) => SetNewTodo(e.target.value)}
+                value={newTodo}
+              />
+              <button
+                className="flex-no-shrink hover:text-teal-600"
+                onClick={addTodo}
+              >
+                <BsFillPlusCircleFill />
+              </button>
+            </div>
+          </div>
+          <div>
+            {todos.map((todo) => (
+              <Todo
+                todo={todo}
+                key={todo._id}
+                completeTodo={completeTodo}
+                deleteTodo={deleteTodo}
+                editTodo={editTodo}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Todo.jsx
@@ -0,0 +1,80 @@
+import { AiFillCheckCircle } from "react-icons/ai";
+import { BsFillTrashFill } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
+import { useState } from "react";
+
+export default function Todo({ todo, completeTodo, deleteTodo, editTodo }) {
+  const [isEditing, SetisEditing] = useState(false);
+  const [editedText, SeteditedText] = useState(todo.text);
+
+  const handleChange = (e) => {
+    SeteditedText(e.target.value);
+  };
+
+  const handleEditbtnClick = () => {
+    SetisEditing(true);
+  };
+  const handleCancelbtnClick = () => {
+    SetisEditing(false);
+    SeteditedText(todo.text);
+  };
+  const handleSubmitbtnClick = () => {
+    editTodo(todo._id, editedText);
+    SetisEditing(false);
+  };
+  return (
+    <>
+      {isEditing ? (
+        <div className="flex mb-4 items-center border-teal-400 shadow appearance-none border-2 rounded p-2">
+          <input
+            className={`w-full text-green focus:outline-none  active:border-0`}
+            onChange={handleChange}
+            value={editedText}
+          />
+
+          <button
+            className="bg-red-500 hover:bg-blue-400 mr-2 text-sm text-white font-bold py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            onClick={handleCancelbtnClick}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-teal-700 hover:bg-green-400 text-sm text-white font-bold py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            onClick={handleSubmitbtnClick}
+          >
+            Submit
+          </button>
+        </div>
+      ) : (
+        <div className="flex mb-4 items-center shadow appearance-none border rounded p-2">
+          {" "}
+          <button className="flex-no-shrink p-1  mr-2 text-grey ">
+            <AiFillCheckCircle
+              onClick={() => completeTodo(todo._id)}
+              className={todo.complete ? "text-green-400 " : ""}
+            />
+          </button>
+          <input
+            className={`w-full ${
+              todo.complete ? "line-through" : ""
+            } text-green focus:outline-none`}
+            value={todo.text}
+            readOnly
+          />
+          <button
+            className="flex-no-shrink p-1  mr-2 text-grey "
+            onClick={handleEditbtnClick}
+          >
+            <FiEdit fontSize={18} className="hover:text-blue-500" />
+          </button>
+          <button
+            className="flex-no-shrink p-1  mr-2 text-grey "
+            onClick={() => deleteTodo(todo._id)}
+          >
+            <BsFillTrashFill fontSize={18} className="hover:text-red-600" />
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
